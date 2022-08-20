bd653cfbd8a2f43761034bc75c73efc4ccfb8367
Diakary TRAORE <diakarytraore@MacBook-Pro-de-Diakary.local>
2022-08-20 23:53:13 +0200

    Renaming environment variable name

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
         mongodb_username: "dev",
         mongodb_password: "Renard+221",
         mongodb_clustername: "cluster0",
-        mongodb_database: "ddd-DB"
+        mongodb_database: "ddd-DB",
+        MONGODB_URI: `mongodb+srv://dev:Renard+221@cluster0.brwnb.mongodb.net/ddd-DB?retryWrites=true&w=majority`
     }
 };
