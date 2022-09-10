715a20a2de60856dbd2b056ae2b3c03907b46824
Diakary TRAORE <diakarytraore@MacBook-Pro-de-Diakary.local>
2022-09-10 17:31:51 +0200

    Preparation of mongodb integration

--- next.config.js
+++ next.config.js
@@ -5,5 +5,17 @@ module.exports = {
         mongodb_clustername: "cluster0",
         mongodb_database: "ddd-DB",
         MONGODB_URI: `mongodb+srv://dev:Renard+221@cluster0.brwnb.mongodb.net/ddd-DB?retryWrites=true&w=majority`
-    }
+    },
 };
+// module.exports = {
+//     webpack: (config, { isServer }) => {
+//         // Fixes npm packages that depend on `fs` module
+//         if (!isServer) {
+//             config.node = {
+//                 fs: 'empty'
+//             }
+//         }
+
+//         return config
+//     }
+// };
--- pages/backoffice/plats/[mealid].js
+++ pages/backoffice/plats/[mealid].js
@@ -1,98 +1,95 @@
-import path from 'path';
-//import fs from 'fs/promises';
+// import path from 'path';
+// import fs from 'fs/promises';
+
 
 import { Fragment } from 'react';
 
-function MealDetailPage(props) {
-    const loadedMeal = props;
+// export const getData = async () => {
+//     const filePath = path.join(process.cwd(), 'data', 'data.json');
+//     const jsonData = await fs.readFile(filePath);
+//     const parsedData = JSON.parse(jsonData);
 
-    // if (!loadedMeal) {
-    //     return <p>Loading...</p>
-    // }
+//     return parsedData;
+// }
+
+
+// export async function getStaticProps(context) {
+//     const { params } = context;
+//     const data = await getData();
+
+//     if (!data) {
+//         return {
+//             notFound: true
+//         }
+//     }
+
+//     const categorieData = data.categories.find(categorie => categorie.name === params.name);
+//     if (!categorieData) {
+//         return {
+//             notFound: true
+//         }
+//     }
+
+//     const mealData = categorieData.meals.find(meal => meal.id === params.id);
+
+//     if (!mealData) {
+//         return {
+//             notFound: true
+//         }
+//     }
+
+//     //TODO : Update meal
+//     //mealData
+
+//     //TODO : Update file
+
+//     return {
+//         props: {
+//             mealData: mealData
+//         },
+//         revalidate: 20
+//     };
+// }
+
+// export async function getStaticPaths(context) {
+//     const { params } = context;
+//     const data = await getData();
+
+//     const categorieData = data.categories.find(categorie => categorie.name === params.name);
+//     if (!categorieData) {
+//         return {
+//             notFound: true
+//         }
+//     }
+
+//     const ids = categorieData.meals.find(meal => meal.id);
+//     const pathWithParams = ids.map((id) => ({ params: { mealid: id } }));
+
+//     return {
+//         // paths: [
+//         //     { params: { mealId: '1519055545-00' } },
+
+//         // ],
+//         paths: pathWithParams,
+//         //fallback: true
+//         fallback: false
+//     }
+// }
+
+const MealDetailPage = (props) => {
+    const { loadedMeal } = props;
+
+    if (!loadedMeal) {
+        return <p>Loading...</p>
+    }
 
     return (
         <Fragment>
-            <h1>{loadedMeal.id}</h1>
+            {/* <h1>{loadedMeal.id}</h1>
             <img src={loadedMeal.image} alt={loadedMeal.name} />
-            <p>{loadedMeal.description}</p>
+            <p>{loadedMeal.description}</p> */}
         </Fragment>
     )
 }
-
-export const getData = async () => {
-    const filePath = path.join(process.cwd(), 'data', 'data.json');
-    //const jsonData = await fs.readFile(filePath);
-    //const parsedData = JSON.parse(jsonData);
-
-    return null;//parsedData;
-}
-
-
-export async function getStaticProps(context) {
-    const { params } = context;
-    const data = await getData();
-
-    if (!data) {
-        return {
-            notFound: true
-        }
-    }
-
-    const categorieData = data.categories.find(categorie => categorie.name === params.name);
-    if (!categorieData) {
-        return {
-            notFound: true
-        }
-    }
-
-    const mealData = categorieData.meals.find(meal => meal.id === params.id);
-
-    if (!mealData) {
-        return {
-            notFound: true
-        }
-    }
-
-    //TODO : Update meal
-    //mealData
-
-    //TODO : Update file
-
-    return {
-        props: {
-            data: {
-                ...mealData
-            }
-        },
-        revalidate: 20
-    };
-}
-
-export async function getStaticPaths(context) {
-    const { params } = context;
-    console.log(params);
-    const data = await getData();
-
-    const categorieData = data.categories.find(categorie => categorie.name === params.name);
-    if (!categorieData) {
-        return {
-            notFound: true
-        }
-    }
-
-    const ids = categorieData.meals.find(meal => meal.id);
-    const pathWithParams = ids.map((id) => ({ params: { mealid: id } }));
-
-    return {
-        // paths: [
-        //     { params: { mealId: '1519055545-00' } },
-
-        // ],
-        paths: pathWithParams,
-        //fallback: true
-        fallback: false
-    }
-}
-
 
 export default MealDetailPage;
