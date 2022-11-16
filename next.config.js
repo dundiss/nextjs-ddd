806c3962beef5d9f035dbd9b15f66a74b5121ce3
Diakary TRAORE <diakarytraore@MacBook-Pro-de-Diakary.local>
2022-11-17 00:46:10 +0100

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,10 @@ module.exports = (phase) => {
                 mongodb_password: "Renard+221",
                 mongodb_clustername: "cluster0",
                 mongodb_database: "ddd-DB-dev",
-                MONGODB_URI: `mongodb+srv://dev:Renard+221@cluster0.brwnb.mongodb.net/ddd-DB?retryWrites=true&w=majority`
+                MONGODB_URI: `mongodb+srv://dev:Renard+221@cluster0.brwnb.mongodb.net/ddd-DB?retryWrites=true&w=majority`,
+                NEXTAUTH_SECRET: "3ef5e0d81d6e18e2fff7ee232be81e8d",
+                NEXT_ATLAS_URI: 'mongodb+srv://dev:Renard+221@cluster0.brwnb.mongodb.net/ddd-DB?retryWrites=true&w=majority',
+                NEXT_ATLAS_DATABASE: 'ddd-DB'
             }
         }
     }
@@ -18,6 +21,7 @@ module.exports = (phase) => {
             mongodb_clustername: "cluster0",
             mongodb_database: "ddd-DB",
             MONGODB_URI: `mongodb+srv://dev:Renard+221@cluster0.brwnb.mongodb.net/ddd-DB?retryWrites=true&w=majority`,
+            NEXTAUTH_SECRET: "3ef5e0d81d6e18e2fff7ee232be81e8d",
             secret: "3ef5e0d81d6e18e2fff7ee232be81e8d",
         }
     }
